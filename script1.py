49faf3a14203d0b353e5835e19ae13685995b0c6
Vivan <vivanvatsa@gmail.com>
2020-09-21 21:55:25 +0530

    Update script1.py

--- script1.py
+++ script1.py
@@ -1,12 +1,16 @@
-"""
-A program that stores the book info:
-TITLE, AUTHOR
-YEAR, ISBN
+from tkinter import *
 
-user can:
-view all the records
-search an entry
-add an entry
-update an entry
-delete
-    """""
+window=Tk()
+l1=Label(window, text='Title')
+l1.grid(row=0, column=0)
+
+l2=Label(window, text='Author')
+l2.grid(row=0, column=2)
+
+l3=Label(window, text='Year')
+l3.grid(row=1, column=0)
+
+l4=Label(window, text='ISBN')
+l4.grid(row=1, column=2)
+
+window.mainloop()
