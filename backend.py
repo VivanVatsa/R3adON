fd1f1d81666dc520212af747f6a277493dd89066
Vivan <vivanvatsa@gmail.com>
2020-09-24 21:52:27 +0530

    Update backend.py

--- backend.py
+++ backend.py
@@ -5,7 +5,7 @@ def connect():
     conn = sqlite3.connect("books.db")
     cur = conn.cursor()
     cur.execute(
-        "CREATE TABLE IF NOT EXISTS book id INTEGER PRIMARY KEY, title text, author text,year integer, isbn integer()")
+        "CREATE TABLE IF NOT EXISTS book (id INTEGER PRIMARY KEY, title text, author text,year integer, isbn integer)")
     conn.commit()
     conn.close()
 
