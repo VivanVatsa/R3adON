da641d6d7e52829c5cb6869f1f82e0ebe94b1eb6
Vivan <vivanvatsa@gmail.com>
2020-09-28 23:25:18 +0530

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -15,6 +15,7 @@ def get_selected_row(event):
     e4.delete(0, END)
     e4.insert(END, selected_tuple[4])
 
+
 def view_command():
     list1.delete(0, END)
     for row in backend.view():
@@ -38,8 +39,11 @@ def add_command():
 def delete_command():
     backend.delete(selected_tuple[0])
 
+
 def update_command():
-    backend.update(selected_tuple[0], selected_tuple[1])
+    backend.update(selected_tuple[0], title_next.get(), author_text.get(),
+                   year_next.get(), isbn_next.get())
+
 
 window = Tk()
 
@@ -97,7 +101,7 @@ b2.grid(row=3, column=3)
 b3 = Button(window, text="Add Entry", width=12, command=add_command)
 b3.grid(row=4, column=3)
 
-b4 = Button(window, text="Update", width=12)
+b4 = Button(window, text="Update", width=12, command=update_command)
 b4.grid(row=5, column=3)
 
 b5 = Button(window, text="Delete", width=12, command=delete_command)
