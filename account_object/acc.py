94c4d5322abae8da601b23d753db3534dd9653c9
Vivan <vivanvatsa@gmail.com>
2020-10-03 13:48:28 +0530

    Update acc.py

--- account_object/acc.py
+++ account_object/acc.py
@@ -3,3 +3,7 @@ class Account:
 # will do file handling embedded here in this code
     def __init__(self, filepath):
         with open(filepath, 'r') as file:
+            self.balance = int(file.read())
+
+account=Account("balance.txt")
+print(account)
