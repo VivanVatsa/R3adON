1d46689350c1970914cf881e7b4f8ccd13bd64e8
Vivan <vivanvatsa@gmail.com>
2020-10-03 22:55:24 +0530

    Update acc.py

--- account_object/acc.py
+++ account_object/acc.py
@@ -4,6 +4,15 @@ class Account:
     def __init__(self, filepath):
         with open(filepath, 'r') as file:
             self.balance = int(file.read())
+    def withdraw(self, amount):
+        self.balance = self.balance - amount
 
+    def deposit(self, amount):
+        self.balance = self.balance + amount
+
+    def commit(self):
+        with expression as target:
+            pass
 account=Account("balance.txt")
-print(account)
+print(account.balance)
+# print(account)
