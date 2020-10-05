adbf62f1fa0617f66ac3fde2bc5015246f62f166
Vivan <vivanvatsa@gmail.com>
2020-10-05 13:34:26 +0530

    Update acc.py

--- account_object/acc.py
+++ account_object/acc.py
@@ -2,6 +2,8 @@
 class Account:
 # will do file handling embedded here in this code
     def __init__(self, filepath):
+        # changing this global variable into instance variable
+        self.filepath = filepath
         with open(filepath, 'r') as file:
             self.balance = int(file.read())
     def withdraw(self, amount):
@@ -11,8 +13,13 @@ class Account:
         self.balance = self.balance + amount
 
     def commit(self):
-        with expression as target:
-            pass
+        with open(self.filepath, 'w') as file:
+            file.write(str(self.balance))
 account=Account("balance.txt")
 print(account.balance)
+# account.withdraw(100)
+# account.withdraw(200)
+account.deposit(400)
+print(account.balance)
+account.commit()
 # print(account)
